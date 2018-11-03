0b4957ad242b1652adfe11f7438145ece03576ef
espacepark <44722962+espacepark@users.noreply.github.com>
2018-11-03 16:22:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function kiusi(){
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   
   const page = await browser.newPage();
-  await page.goto('https://rockandred.github.io/lukapa/index.html'); 
+  await page.goto('https://kinhhen862.github.io/chayt/aaa.html'); 
   console.log('----XONG0--------');
   setTimeout(async () => await await page.close(), 185000);
   setTimeout(async () => await browser.close(), 193000);
